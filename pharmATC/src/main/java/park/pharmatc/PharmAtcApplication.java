9cfcc846effc73ed2fd62efc15fcd1164c853c7b
박민성 <pascalmin49@gmail.com>
2025-06-17 18:42:10 +0900

    [init] 초기 코드 설정

---
+++ pharmATC/src/main/java/park/pharmatc/PharmAtcApplication.java
@@ -0,0 +1,13 @@
+package park.pharmatc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PharmAtcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PharmAtcApplication.class, args);
+    }
+
+}
