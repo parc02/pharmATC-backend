0b876fa342940020b1eef07d78fc08cf2f88f860
박민성 <pascalmin49@gmail.com>
2025-06-21 20:02:49 +0900

    코드 수정'
    ''

--- pharmATC_Backend/pharmATC/src/main/java/park/pharmatc/v1/repository/DrugItemRepository.java
+++ pharmATC_Backend/pharmATC/src/main/java/park/pharmatc/v1/repository/DrugItemRepository.java
@@ -2,6 +2,7 @@ package park.pharmatc.v1.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import park.pharmatc.v1.entity.DrugItemEntity;
 
 import java.util.List;
@@ -9,14 +10,25 @@ import java.util.Optional;
 
 public interface DrugItemRepository extends JpaRepository<DrugItemEntity, Long> {
 
-    Optional<DrugItemEntity> findByItemSeq(String itemSeq);
+    @Query("SELECT di FROM DrugItemEntity di " +
+            "LEFT JOIN FETCH di.company " +
+            "LEFT JOIN FETCH di.dimensions " +
+            "LEFT JOIN FETCH di.image " +
+            "WHERE di.ediCode = :ediCode")
+    List<DrugItemEntity> findByEdiCodeWithAll(@Param("ediCode") String ediCode);
 
-    @Query("SELECT d.itemSeq FROM DrugItemEntity d")
-    List<String> findAllItemSeqs();
+    @Query("SELECT di FROM DrugItemEntity di " +
+            "LEFT JOIN FETCH di.company " +
+            "LEFT JOIN FETCH di.dimensions " +
+            "LEFT JOIN FETCH di.image " +
+            "WHERE di.itemName LIKE %:itemName%")
+    List<DrugItemEntity> findByItemNameContainingWithAll(@Param("itemName") String itemName);
 
-    @Query("SELECT i FROM DrugItemEntity i " +
-            "LEFT JOIN FETCH i.company " +
-            "LEFT JOIN FETCH i.image " +
-            "LEFT JOIN FETCH i.dimensions")
+    @Query("SELECT di FROM DrugItemEntity di " +
+            "LEFT JOIN FETCH di.company " +
+            "LEFT JOIN FETCH di.dimensions " +
+            "LEFT JOIN FETCH di.image")
     List<DrugItemEntity> findAllWithAssociations();
+
+    Optional<DrugItemEntity> findByItemSeq(String itemSeq);
 }
