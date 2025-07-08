429b2417daa0e6c2eb3032a5ba7efe49ae5bf229
minseongpark <pascalmin49@gmail.com>
2025-07-08 19:09:19 +0900

    webConfig 수정

--- src/main/java/park/pharmatc/v1/WebConfig.java
+++ src/main/java/park/pharmatc/v1/WebConfig.java
@@ -12,7 +12,9 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addMapping("/api/**")
                 .allowedOrigins(
                         "http://localhost:3000",               // 로컬 개발용 (필요시 제거 가능)
-                        "https://pharmatc-90ac0.web.app"       // Firebase 프로덕션
+                        "https://pharmatc-90ac0.web.app",
+                        "http://3.25.208.164"
+                        // Firebase 프로덕션
                 )
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
